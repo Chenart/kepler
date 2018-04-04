b5f2ef267d9afe9257d481e275eca1c446c8beee
jzaralim <jzaralim@gmail.com>
2018-04-04 13:37:05 -0700

    fix map paths, make the map EVEN BIGGER

--- map/map.py
+++ map/map.py
@@ -10,7 +10,7 @@ from io import BytesIO
 import math
 
 # phi, theta
-orbitals = [[0, 0], [60, 45], [34, 60], [24,80]]
+num_orbitals = 100
 
 class Example(Frame):
     def __init__(self, parent):
@@ -18,14 +18,11 @@ class Example(Frame):
         self.parent = parent
 
         self.orbital = 0
-        self.curr_long = 0
-        self.curr_lat = 0
-        self.vspeed = 0
-        self.hspeed = 0
+        self.curr_time = 0
 
         self._job = None
 
-        self.image = Image.open('map.jpg')
+        self.image = Image.open('bigmap.jpg')
         self.width, self.height = self.image.size
         self.image.crop((0,0,1,1))
 
@@ -49,32 +46,29 @@ class Example(Frame):
         self.pack(fill=BOTH, expand=1)
 
     def pan(self):
+        init_x = self.width * self.orbital/(2.0 * num_orbitals)
+        curr_x = init_x + 2 * self.curr_time
+        curr_y = 0.5 * self.height + 0.5 * self.height * math.sin(2 * math.pi * (curr_x - init_x)/self.width)
+        if curr_x < 200:
+            curr_x = 200
+        elif curr_x > self.width - 200:
+            curr_x = 200
+
+        if curr_y < 200:
+            curr_y = 200
+        elif curr_y > self.height - 200:
+            curr_y = self.height - 200
         img = ImageTk.PhotoImage(self.image.crop(
             (
-                self.curr_long - 100,
-                self.curr_lat - 100,
-                self.curr_long + 100,
-                self.curr_lat + 100)
+                curr_x - 200,
+                curr_y - 200,
+                curr_x + 200,
+                curr_y + 200)
             ))
 
         self.panel.configure(image = img)
         self.panel.image = img
-
-        self.curr_long += self.vspeed
-        self.curr_lat += self.hspeed
-
-        if self.curr_long < 100:
-            self.curr_long = self.height - 100
-
-        if self.curr_long > self.height - 100:
-            self.curr_long = 100
-
-        if self.curr_lat < 100:
-            self.curr_lat = self.width - 100
-
-        if self.curr_lat > self.width - 100:
-            self.curr_lat = 100
-
+        self.curr_time += 1
 
         self._job = self.panel.after(10, self.pan)
 
@@ -88,11 +82,7 @@ class Example(Frame):
             self.panel.after_cancel(self._job)
             self._job = None
         self.orbital =  int(self.selector.get())
-
-        self.curr_lat = self.height/2.0
-        self.curr_long = self.width * (0.5 + math.sin(orbitals[self.orbital][0])/2)
-        self.vspeed = 2 * math.sin(orbitals[self.orbital][1])
-        self.hspeed = 2 * math.cos(orbitals[self.orbital][1])
+        self.curr_time = 0
 
         self.pan()
 
@@ -100,7 +90,7 @@ class Example(Frame):
 
 def main():
 	root = Tk()
-	root.geometry("1000x900+300+300")
+	root.geometry("700x700+300+300")
 	app = Example(root)
 	root.mainloop()  
 
